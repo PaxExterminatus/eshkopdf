410ba1565a899d675f103827ae98cd61bfe24d06
voronin <paxexterminatus@gmail.com>
2017-09-11 19:50:19 +0300

    update

--- integrator.js
+++ integrator.js
@@ -1,13 +1,14 @@
-var pdfPath = document.getElementsByClassName('pdfview')[0].getAttribute('data-pdf-name');
+var pdfPath
 
 var pdfjs_block = document.getElementsByClassName('pdfview')[0];
 var pdfFrame = document.createElement('iframe');
 pdfFrame.src = 'http://www.eshko.by/source_lib/eshkopdf/viewer/mozilla/viewer.html';
 pdfFrame.name = pdfPath;
+
 pdfjs_block.appendChild(pdfFrame);
 
 var head = document.getElementsByTagName('head')[0];
 var link = document.createElement('link');
 link.rel="stylesheet";
-link.href="style.css";
+link.href="http://www.eshko.by/source_lib/eshkopdf/style.css";
 head.appendChild(link);
